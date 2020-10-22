5a5eb252a79d1792318981f73c365de6e3b25e4f
katarina93petrovic <katarina93petrovic@gmail.com>
2020-10-22 23:51:07 +0200

    RegExp changes

--- script/script.js
+++ script/script.js
@@ -12,26 +12,57 @@ function addBookToLibrary(title, author, pages, read) {
   library.push(new createBook(title, author, pages, read));
 }
 
-//Korisnicki input, dodavanje novih knjiga
 var readingList = document.querySelector(".readingList");
 var completedBooks = document.querySelector(".completedBooks");
 var form = document.querySelector("#addNewBook form");
 var radio;
 var alert = document.getElementById("success_alert");
+var ispisGreske = document.getElementById('pogresanUnos');
 
+//Dodavanje novih knjiga i validacija unosa preko RegExp
 form.addEventListener("submit", (e) => {
-  radio = document.querySelector("input[name='customRadio']:checked");
-  addBookToLibrary(
-    e.target[0].value,
-    e.target[1].value,
-    e.target[2].value,
-    radio.value
-  );
-  e.preventDefault();
-  form.reset();
-  display();
-  alert.style.opacity = "100%";
-  setTimeout(() => document.querySelector("#success_alert").remove(), 3000);
+  let nazivKnjige = document.getElementById('inputTitle');
+  let nazivKnjigeVrednost = nazivKnjige.value;
+  let regexNazivKnjige = /^[A-ZČĆŽŠĐ0-9-_\.]{1}[a-zčćžšđ0-9-_\.]{0,20}(\s[a-zčćžšđA-ZČĆŽŠĐ0-9-_\.]{0,20})*$/;
+  let autorKnjige = document.getElementById('autor');
+  let autorVrednost = autorKnjige.value;
+  let regexImeAutora = /^[A-ZČĆŽŠĐ]{1}[a-zčćžšđ\.]{0,20}(\s[A-ZČĆŽŠĐ]{1}[a-zčćžšđ0-9\.]{0,20})*$/;
+  let stranice = document.getElementById('numberOfPages');
+  let straniceVrednost = stranice.value;
+  let regexBrStranica = /^[0-9]{1,4}$/;
+
+  let validanNaziv = regexNazivKnjige.test(nazivKnjigeVrednost);
+  let validanAutor = regexImeAutora.test(autorVrednost);
+  let validanBrStr = regexBrStranica.test(straniceVrednost);
+
+  if(validanNaziv==false){
+    ispisGreske.innerHTML = "Niste dobro uneli naziv knjige!";
+    ispisGreske.style.opacity = '100%'
+  }
+  else if(validanAutor==false) {
+    ispisGreske.innerHTML = "Niste dobro uneli ime autora!";
+    ispisGreske.style.opacity = '100%'
+  }
+  else if(validanBrStr==false){
+    ispisGreske.innerHTML = "Niste dobro uneli broj stranica!";
+    ispisGreske.style.opacity = '100%'
+  }
+  else{
+     radio = document.querySelector("input[name='customRadio']:checked");
+    
+        addBookToLibrary(
+          e.target[0].value,
+          e.target[1].value,
+          e.target[2].value,
+          radio.value
+        );
+      e.preventDefault();
+      form.reset();
+      display();
+      alert.style.opacity = "100%";
+      setTimeout(() => document.querySelector("#success_alert").remove(), 3000);
+      ispisGreske.style.opacity = "0%";
+    }
 });
 
 //Ispis na stranici
